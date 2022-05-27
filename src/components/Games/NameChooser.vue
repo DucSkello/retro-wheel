86d35d2a9f5eba14c43b379dc37cccbba2b7e6bf
MaximeSkello <maxime@skello.io>
2022-05-27 16:44:53 +0200

    added tailwind css

--- src/components/Games/NameChooser.vue
+++ src/components/Games/NameChooser.vue
@@ -1,17 +1,31 @@
 <template>
   <section>
-    <div>
-      <input v-model="name">
-      <button @click.prevent="handleSave()">
-        save name
-      </button>
-    </div>
-    <div>
-      <!-- <tr v-for="(player, i) in players" :key="i">
-        <td>
-          {{ player }}
-        </td>
-      </tr> -->
+      <!-- <div>
+        <input v-model="name">
+      </div> -->
+      <div class="w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+              Enregistrer un blaze 
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+              id="username" 
+              type="text" 
+              placeholder="Username"
+              v-model="name"
+            >
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              @click.prevent="handleSave()"
+            >
+              save name
+            </button>
+          </div>
+        </form>
+      </div>
+      <div>
     </div>
   </section>
 </template>
