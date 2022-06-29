f461cce7be3a55567a9ce57593a86edfabbed5eb
Duc Nguyen <duc@skello.io>
2022-06-29 12:08:55 +0200

    add a package-lock
    remove public path
    
    Revert "remove public path"
    
    This reverts commit cb761400e170a7885c09d7fa32fad14aea5bc67f.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'retro-wheel',
+  publicPath: '/retro-wheel/',
   transpileDependencies: true,
   runtimeCompiler: true
 })
